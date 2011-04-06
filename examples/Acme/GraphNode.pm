b3ab4b82f309f3a746a6e85021108510f575d3f0
Michael DeHaan <michael.dehaan@gmail.com>
2011-04-06 18:31:59 -0400

    Add missing file needed for Graphinator demo.

---
+++ examples/Acme/GraphNode.pm
@@ -0,0 +1,21 @@
+# example Graph Node class, for illustrating Graphinator.  This could mixin either NoSql or DbTable.
+# here it doesn't do either because it is just for the tests.
+
+use MooseX::Declare;
+
+class Acme::GraphNode extends Acme::BaseObject {
+ 
+    use Method::Signatures::Simple name => 'action';
+    use Acme::BaseObject;
+
+    # database fields are all marked with 'data'
+    data x            => (isa => 'Int');
+    data y            => (isa => 'Int');
+    data z            => (isa => 'Int');
+
+    # what key to use for graph indexing
+    action node_key() {
+        return "GraphNode:" . $self->x();
+    }
+
+}
