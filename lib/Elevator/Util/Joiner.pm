f60358af41cf884df7e87e36b1f292e16233c465
Michael DeHaan <Michael DeHaan michael.dehaan@gmail.com>
2011-06-03 14:30:12 -0400

    Remove warnings

--- lib/Elevator/Util/Joiner.pm
+++ lib/Elevator/Util/Joiner.pm
@@ -49,7 +49,6 @@ class Elevator::Util::Joiner extends Elevator::Model::BaseObject {
                $self->_all_fields(),
                $self->where()
          );
-         warn "GENERATED SELECT IS THUS: $select\n";
          # obviously this means no joins across seperate database connections :)
          # TODO: assert that we have at least two classes/tables to join
          my $sth = $self->unite->[0]->[1]->sql_driver->database_handle->prepare($select);
@@ -63,16 +62,12 @@ class Elevator::Util::Joiner extends Elevator::Model::BaseObject {
          my $i = 0;
  
          foreach my $row (@$data) {
-              my $translated_row = $self->_arrayref_to_hashref($row, $self->_table_names()->[$i]);
-              my $i++;
-              push @$all_rows, $translated_row;
+              push @$all_rows, $self->_arrayref_to_hashref($row, $self->_table_names()->[$i++]);
          }
 
          my $results = [];
          foreach my $row2 (@$all_rows) {
-             warn "TRANSATED ROW = " . JSON::XS::encode_json($row2) . "\n";
-             my $objects = $self->_convert_to_objects($self->_partitioned_hashrefs($row2));
-             push @$results, $self->stitching->($objects);
+             push @$results, $self->stitching->($self->_convert_to_objects($self->_partitioned_hashrefs($row2)));
          }
 
          return $results;
@@ -108,8 +103,7 @@ class Elevator::Util::Joiner extends Elevator::Model::BaseObject {
         my $i = 0;
         my $results = [];
         foreach my $item (@{$self->unite()}) {
-           push @$results, $table_names[$i] . ' ' . $item->[0];
-           $i++;
+           push @$results, $table_names[$i++] . ' ' . $item->[0];
         }
         return $results;
     }
@@ -120,13 +114,11 @@ class Elevator::Util::Joiner extends Elevator::Model::BaseObject {
         my $i = 0;
         foreach my $class (@{$self->_class_names()}) {
            my $attributes = $class->new()->my_attributes();
-           my $call_name = $self->_call_names()->[$i];
+           my $call_name = $self->_call_names()->[$i++];
            foreach my $attr (@$attributes) {
                push @$results, $call_name . '.' . $attr->field() if $attr->does('Elevator::Model::Traits::Data');
            }
-           $i++;
         }
-        warn JSON::XS::encode_json($results);
         return $results;
     }
 
@@ -137,26 +129,18 @@ class Elevator::Util::Joiner extends Elevator::Model::BaseObject {
         my $i=0;
         my $results = [];
         foreach my $item (@$class_names) {
-            warn "CONVERTING " . JSON::XS::encode_json($list_of_hashrefs->[$i]) . " to " . $item . "\n";
-            my $obj = $item->from_datastruct($list_of_hashrefs->[$i++]);
-            warn "OBJECTIFIED: " . $obj->to_datastruct();
-            push @$results, $obj;
-
+            push @$results, $item->from_datastruct($list_of_hashrefs->[$i++]);
         }
         return $results;
     }
     
     # { a.foo => 1, a.bar => 2, b.cat => 3} -> [ { foo => 1, bar => 2 }, { cat => 3} ]
     action _partitioned_hashrefs($row_hashref) {
-        warn "INPUT = " . JSON::XS::encode_json($row_hashref) . "\n";
         my $bucketized = {};
         my @keyz = keys(%$row_hashref);
         foreach my $key (@keyz) {
             my $value = $row_hashref->{$key};
             my ($called_as, $field) = split /\./, $key;
-            #warn "CALLED AS = $called_as\n";
-            #warn "FIELD     = $field\n";
-            #warn "VALUE     = $value\n";
             $bucketized->{$called_as} ||= {};
             $bucketized->{$called_as}->{$field} = $value;
         }
@@ -166,7 +150,6 @@ class Elevator::Util::Joiner extends Elevator::Model::BaseObject {
         foreach my $item (@{$self->_call_names()}) {
             push @$results, $bucketized->{$item};
         }
-        warn "partitioned in order = " . JSON::XS::encode_json($results); 
         return $results;
 
 
